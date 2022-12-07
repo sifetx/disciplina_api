46359e17913aba7b99a61c3eae46bfce529542c4
sifet <54966900+sifetx@users.noreply.github.com>
2022-12-07 00:25:49 +0000

    FT2 ex1

---
+++ FT2/Ficha2exe1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <conio.h>
+#include <locale.h>
+
+main() {
+    int num1, num2, soma, sub, mult;
+    float div;
+    setlocale(LC_ALL, "portuguese");
+    wprintf(L"Insira dois números: ");
+    scanf("%d%d",&num1,&num2);
+    soma = num1 + num2;
+    sub = num1 - num2;
+    mult = num1 * num2;
+    div = (float)num1 / (float)num2;
+    wprintf(L"A soma é %d\n", soma);
+    wprintf(L"A subtração é %d\n", sub);
+    wprintf(L"A multiplicação é %d\n", mult);
+    wprintf(L"A divisão é %.2f\n", div);
+}
