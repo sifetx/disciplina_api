f4576e8f0fa12f45e7d83dc996deddeb96b75838
sifet <54966900+sifetx@users.noreply.github.com>
2022-12-07 17:42:32 +0000

    simbolo de cm ao cubo

--- FT2/Ficha2exe3.c
+++ FT2/Ficha2exe3.c
@@ -3,12 +3,14 @@
 #include <stdio.h>
 #include <math.h>
 #include <locale.h>
+#include <wchar.h>
 
 float aresta;
+wchar_t cubo = 0x00B3;   //simbolo do cubo
 
 main() {
     setlocale(LC_ALL, "portuguese");
     wprintf(L"Digita a medida das arestas do cubo (cm): ");
     scanf("%f", &aresta);
-    wprintf(L"A área das faces desse cubo é: %.2fcm³", (pow(aresta,2)*6));
+    wprintf(L"A área das faces desse cubo é: %.2fcm%lc", (pow(aresta,2)*6), cubo);
 }
