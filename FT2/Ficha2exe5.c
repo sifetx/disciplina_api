68301cf78aa63050453e5be45481d6a690d1f067
sifet <54966900+sifetx@users.noreply.github.com>
2022-12-08 01:27:37 +0000

    Ficha 2 Ex5

---
+++ FT2/Ficha2exe5.c
@@ -0,0 +1,17 @@
+//Preço por m²
+
+#include <stdio.h>
+#include <locale.h>
+#include <wchar.h>
+
+wchar_t quadrado = 0x00B2;   //simbolo do quadrado
+float preco, area;
+
+main() {
+    setlocale(LC_ALL, "portuguese");
+    wprintf(L"Introduza o preço por m%lc de terreno: ", quadrado);
+    scanf("%f", &preco);
+    wprintf(L"Introduza a área do terreno: ");
+    scanf("%f", &area);
+    wprintf(L"O preço por m%lc deste terreno é %.2f", quadrado, preco/area);
+}
